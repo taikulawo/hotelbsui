6b59e996639403427723c509ff716203f9b62bce
iamwwc <qaq1362211689@gmail.com>
2020-05-24 09:41:11 +0800

    准备添加登录，master分支暂不影响

--- src/router.tsx
+++ src/router.tsx
@@ -14,6 +14,7 @@ import Consumer from './components/consumer';
 import Home from './components/home'
 import Staff from './components/staff';
 import Room from './components/room';
+import Login from './components/login'
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 export default connect()(class extends React.Component {
@@ -31,71 +32,85 @@ export default connect()(class extends React.Component {
       <React.Fragment>
         <Router>
           <React.Fragment>
-            <Layout style={{ minHeight: '100vh' }}>
-              <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse} style={{
-                background: '#304156'
-              }}>
-                <div className="logo" />
-                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" style={{
-                  userSelect: 'none',
-                  paddingTop: '64px'
-                }} selectedKeys={[window.location.pathname]}>
-                  <Menu.Item key="/">
-                    <Link to="/">
-                      <PieChartOutlined />
-                      <span>主页</span>
-                    </Link>
-                  </Menu.Item>
-                  <SubMenu
-                    key="/user"
-                    title={
-                      <span>
-                        <Link to="/user" style={{
-                          display: 'block'
-                        }}>
-                          <PieChartOutlined />
-                          <span>用户</span>
-                        </Link>
-                      </span>
-                    }
-                  >
-                    <Menu.Item key="/user/consumers">
-                      <Link to="/user/consumers">
-                        <PieChartOutlined />
-                        <span>旅客</span>
-                      </Link>
-                    </Menu.Item>
-                    <Menu.Item key="/user/staffs">
-                      <Link to="/user/staffs">
-                        <PieChartOutlined />
-                        <span>员工</span>
-                      </Link></Menu.Item>
-                  </SubMenu>
-                  <Menu.Item key="/room">
-                    <Link to="/room">
-                      <PieChartOutlined />
-                      <span>房间类型</span>
-                    </Link>
-                  </Menu.Item>
-                </Menu>
-              </Sider>
-              <Layout className="site-layout">
-                <Header className="site-layout-background" style={{ padding: 0 }} />
-                <Content className="display-content" style={{ margin: '0 16px' }}>
-                  <Switch>
-                    <Route path="/user" component={User} exact></Route>
-                    <Route path="/user/consumers" exact component={Consumer}></Route>
-                    <Route path="/user/staffs" exact component={Staff}></Route>
-                    <Route path="/" exact component={Home}></Route>
-                    <Route path="/room" component={Room}></Route>
-                  </Switch>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>Created by wuweichao</Footer>
-              </Layout>
-            </Layout>
+            {
+              userIsLogin ? (
+                <Login></Login>
+              ) : (
+                  <Layout style={{ minHeight: '100vh' }}>
+                    <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse} style={{
+                      background: '#304156'
+                    }}>
+                      <div className="logo" />
+                      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" style={{
+                        userSelect: 'none',
+                        paddingTop: '64px'
+                      }} selectedKeys={[window.location.pathname]}>
+                        <Menu.Item key="/">
+                          <Link to="/">
+                            <PieChartOutlined />
+                            <span>主页</span>
+                          </Link>
+                        </Menu.Item>
+                        <SubMenu
+                          key="/user"
+                          title={
+                            <span>
+                              <Link to="/user" style={{
+                                display: 'block'
+                              }}>
+                                <PieChartOutlined />
+                                <span>用户</span>
+                              </Link>
+                            </span>
+                          }
+                        >
+                          <Menu.Item key="/user/consumers">
+                            <Link to="/user/consumers">
+                              <PieChartOutlined />
+                              <span>入住旅客</span>
+                            </Link>
+                          </Menu.Item>
+                          <Menu.Item key="/user/staffs">
+                            <Link to="/user/staffs">
+                              <PieChartOutlined />
+                              <span>酒店员工</span>
+                            </Link>
+                          </Menu.Item>
+                        </SubMenu>
+                        <Menu.Item key="/room">
+                          <Link to="/room">
+                            <PieChartOutlined />
+                            <span>房间类型</span>
+                          </Link>
+                        </Menu.Item>
+                      </Menu>
+                    </Sider>
+                    <Layout className="site-layout">
+                      <Header className="site-layout-background" style={{ padding: 0 }} />
+                      <Content className="display-content" style={{ margin: '0 16px' }}>
+                        <Switch>
+                          <Route path="/user" component={User} exact></Route>
+                          <Route path="/user/consumers" exact component={Consumer}></Route>
+                          <Route path="/user/staffs" exact component={Staff}></Route>
+                          <Route path="/" exact component={Home}></Route>
+                          <Route path="/room" component={Room}></Route>
+                        </Switch>
+                      </Content>
+                      <Footer style={{ textAlign: 'center' }}>Created by wuweichao</Footer>
+                    </Layout>
+                  </Layout>
+                )
+            }
           </React.Fragment>
         </Router>
-      </React.Fragment>
+      </React.Fragment >
     );
   }
 })
+
+export function userIsLogin(): boolean {
+  if (document.cookie.indexOf("jwt_token") !== -1) {
+    return true
+  }
+  return false
+}
